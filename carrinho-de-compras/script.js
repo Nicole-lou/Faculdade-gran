473b1f6575a1f1fe90831f207d5fa1797fa6e12c
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-03-01 22:33:38 -0300

    Js pronto

---
+++ carrinho-de-compras/script.js
@@ -0,0 +1,34 @@
+function alterarQtd(produto, acao) {
+    const qtd = document.getElementById('quantidade_' + produto)
+    const valor = document.getElementById('valor_' + produto)
+    const total = document.getElementById('total_' + produto)
+
+    if(acao == '-' && qtd.innerHTML == 0) {
+    alert('A quantidade não pode ser menor que 0')
+    } else {
+        acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
+      const valorTotal = qtd.innerHTML * somenteNumeros(valor.innerHTML)
+        total.innerHTML = formatarValor(valorTotal)
+        soma()
+    }
+
+   
+
+}
+
+function soma () {
+    for(let i = 1; i < 4; i++) {
+        let numero = somenteNumeros( document.getElementById('total_' + i).innerHTML)
+        soma += Number(numero)
+    }
+
+    document.getElementById('subtotal').innerHTML = formatarValor(soma)
+}
+
+function somenteNumeros(n){
+    return n.replace(/\D/g, '')
+}
+
+function formatarValor(n) {
+    return 'R$ ' + n.toLocaleString('PT-BR')
+}
